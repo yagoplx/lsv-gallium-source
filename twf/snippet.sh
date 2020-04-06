86a6efa7906772c7dbef8577393af2c3c9600739
yagoplx <57966308+yagoplx@users.noreply.github.com>
2020-04-06 01:25:15 -0300

    delete a leftover snippet (an experiment)

--- twf/snippet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-inotifywait -m /home/mcs/snapServer/runtime/"Mineland v1"/stats -e create -e moved_to |
-    while read dir action file; do
-        echo "The file '$file' appeared in directory '$dir' via '$action'"
-        # do something with the file
-    done
